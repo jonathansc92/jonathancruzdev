384c73618dcc6d8d454401b710b4c41e94cb16e6
Jonathan Silva Cruz <jonathansc92@gmail.com>
2021-03-27 04:17:22 -0300

    fix width portfolio

--- src/components/PortfolioComponent.js
+++ src/components/PortfolioComponent.js
@@ -17,7 +17,7 @@ class PortfolioComponent extends React.Component {
         <>
             <div className={`col-lg-4 col-md-6 portfolio-item filter-${this.state.filter}`}>
                 <div className="portfolio-wrap">
-                    <img style={{ width: '320px', height: '180px' }} src={this.state.image} className="img-fluid" alt={this.state.title} />
+                    <img style={{ width: '100%', height: '180px' }} src={this.state.image} className="img-fluid" alt={this.state.title} />
                     <div className="portfolio-links">
                       {/* <a href={this.state.image} data-gallery="portfolioGallery" className="portfolio-lightbox" title="App 1"><i className="bx bx-plus"></i></a> */}
                       <a style={{ width: '100%' }} href={this.state.link} title="Mais detalges" target="_blank" rel="noreferrer"><i className="bx bx-link"></i></a>
